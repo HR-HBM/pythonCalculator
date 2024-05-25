4b46ca58e7e2f9878b7ca17d491b52ade04c4125
Humaira Muzammil <humaihr469@gmail.com>
2024-05-25 17:36:10 +0530

    complete While loop for project

--- main.py
+++ main.py
@@ -1,62 +1,69 @@
-print("Welcome!")
+repeat = True
+while repeat:
 
-x = 0
-y = 0
-# calculation = 0
+    print("Welcome!")
 
-#calculator functions
+    # x = 0
+    # y = 0
+    # calculation = 0
 
-def add(x,y):
-    return x + y
+    #calculator functions
 
-def subtract(x,y):
-    return x - y
+    def add(x,y):
+        return x + y
 
-def multiply(x,y):
-    return x * y
+    def subtract(x,y):
+        return x - y
 
-def divide(x,y):
-    return x / y
+    def multiply(x,y):
+        return x * y
 
-
-#calculator operator dictionary
-
-operations = {
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "/": divide
-}
-
-x = int(input("Enter the first number: "))
-def continue_calculation():
-    global x
-
-    operator = input("Select the operator +, -, *, /: ")
-    y = int(input("Enter the next number: "))
-
-    if operator in operations:
-        print(f"Operator: {operator},  value: {operations[operator]}")
-
-    else:
-        print("invalid operator!")
-
-    calculation = operations[operator](x,y)
-    print(calculation)
-
-    print(f"{x} {operator} {y} = " + str(calculation))
-
-    x = calculation
-
-continue_calculation()
+    def divide(x,y):
+        return x / y
 
 
-user_choice = input("Enter 'yes' to continue calculating or enter 'no' to exit.").lower()
+    #calculator operator dictionary
 
-while user_choice == "yes":
+    operations = {
+        "+": add,
+        "-": subtract,
+        "*": multiply,
+        "/": divide
+    }
+
+
+
+
+    x = int(input("Enter the first number: "))
+    def continue_calculation():
+        global x
+
+        operator = input("Select the operator +, -, *, /: ")
+        y = int(input("Enter the next number: "))
+
+        if operator in operations:
+            print(f"Operator: {operator},  value: {operations[operator]}")
+
+        else:
+            print("invalid operator!")
+
+        calculation = operations[operator](x,y)
+        # print(calculation)
+
+        print(f"{x} {operator} {y} = " + str(calculation))
+
+        x = calculation
+
     continue_calculation()
-    user_choice = input("Enter 'yes' to continue calculating or enter 'no' to exit.").lower()
-if user_choice == "no":
-    print("end")
+
+
+    user_choice = input("Enter 'yes' to continue calculating or enter 'no' to exit.")
+
+    while user_choice.lower() == "yes":
+        continue_calculation()
+        user_choice = input("Enter 'yes' to continue calculating or enter 'no' to exit.")
+    if user_choice.lower() == "no":
+        repeat = True
+        continue
 
 
